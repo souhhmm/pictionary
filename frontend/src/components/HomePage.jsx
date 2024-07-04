cf60642b6bc7ce2ed0a98077bab09c8318570f92
Soham <kalburgisoham@gmail.com>
2024-07-04 17:36:57 +0530

    updated outline

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function HomePage() {
-  const [roomID, setRoomID] = useState('');
+  const [roomID, setRoomID] = useState("");
+  const [copied, setCopied] = useState(false);
 
   const generateRoomID = () => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    let result = '';
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let result = "";
     for (let i = 0; i < 8; i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
@@ -14,7 +15,12 @@ export default function HomePage() {
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(roomID);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
   };
+
   return (
     <>
       <div className="flex justify-center">
@@ -30,16 +36,16 @@ export default function HomePage() {
         <div className="mx-2 my-2">
           <label>Create Room</label>
           <div>
-            <input type="text" className="mx-2 my-2 border-2 " placeholder="Name"></input>
+            <input type="text" className="mx-2 my-2 border-2" placeholder="Name"></input>
           </div>
           <div>
-            <input type="text" className="mx-2 border-2" placeholder="Generate Room ID" 
-            value={roomID} onClick={copyToClipboard} readOnly></input>
-            <button className="border-2" onClick={generateRoomID}>Generate</button>
+            <input type="text" className="mx-2 border-2" placeholder="Generate Room ID" value={copied ? "Copied!" : roomID} onClick={copyToClipboard} readOnly></input>
+            <button className="border-2" onClick={generateRoomID}>
+              Generate
+            </button>
           </div>
         </div>
       </div>
-
     </>
   );
 }
