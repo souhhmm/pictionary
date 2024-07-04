271a8c7b691b43a3e473b1410e2e01e4e7b43124
Soham <kalburgisoham@gmail.com>
2024-07-04 16:38:34 +0530

    gitignore

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,7 @@ app.get("/", (req, res) => {
     res.send("Server is running");
 });
 
+io.on("connection", (socket) => { console.log("A user connected") });
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {console.log(`Server running on port ${PORT}`)});
+server.listen(PORT, () => { console.log(`Server running on port ${PORT}`) });
